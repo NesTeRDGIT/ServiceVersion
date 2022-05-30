57bf35b621943aa7c2f237a8c9e5a0fedd7ec534
aam <aam@MAA-OIO>
2022-05-30 12:18:02 +0900

    итоговый реестр в счетах фактур готово

--- ClientServiceWPF/ExportSchetFactureFile/ExportShetFactureFileViewModel.cs
+++ ClientServiceWPF/ExportSchetFactureFile/ExportShetFactureFileViewModel.cs
@@ -137,10 +137,12 @@ namespace ClientServiceWPF.ExportSchetFactureFile
 
                 foreach (DataRow row in dtSMO.Rows)
                 {
-                    var smoName = row["smocod"].ToString();
-                    AddLogs(LogType.Info, $"Формирование случаев CMO {smoName}");
+                    var smoCode = row["smocod"].ToString();
+                    var smoName = row["nam_smok"].ToString();
 
-                    var path = Path.Combine(fbd.SelectedPath, smoName);
+                    AddLogs(LogType.Info, $"Формирование случаев CMO {smoCode}");
+
+                    var path = Path.Combine(fbd.SelectedPath, smoCode);
                     if (!Directory.Exists(path))
                         Directory.CreateDirectory(path);
 
@@ -149,13 +151,13 @@ namespace ClientServiceWPF.ExportSchetFactureFile
                     tasks.Add(Task.Run(() =>
                     {
                         AddLogs(LogType.Info, $"Формирование итоговых реестров");
-                        ItogReestrXls(path, smoName, CurrentDate, cts.Token);
+                        ItogReestrXls(path, $"{smoCode}-{smoName}", CurrentDate, cts.Token);
                     }));
 
                     tasks.Add(Task.Run(() =>
                     {
                         AddLogs(LogType.Info, $"Формирование счетов фактур");
-                        SchetFactureXls(path, smoName, CurrentDate, cts.Token);
+                        SchetFactureXls(path, $"{smoCode}-{smoName}", CurrentDate, cts.Token);
                     }));
 
                     await Task.WhenAll(tasks);
@@ -170,12 +172,14 @@ namespace ClientServiceWPF.ExportSchetFactureFile
             }
         }
 
-        private void SchetFactureXls(string path, string smoName, DateTime currentDate, CancellationToken token)
+        private void SchetFactureXls(string path, string smoData, DateTime currentDate, CancellationToken token)
         {
+            var smoCode = smoData.Split('-')[0];
+            var smoName = smoData.Split('-')[1];
             var dtFakture = new DataTable();
             using (var conn = new OracleConnection(AppConfig.Property.ConnectionString))
             {
-                using (var oda = new OracleDataAdapter($"select * from FAKTURA_2021 t where smo = {smoName}", conn))
+                using (var oda = new OracleDataAdapter($"select * from FAKTURA_2021 t where smo = {smoCode}", conn))
                 {
                     dispatcher.Invoke(() => { Progress1.Text = "Запрос данных FAKTURA_2021"; });
                     oda.Fill(dtFakture);
@@ -184,12 +188,15 @@ namespace ClientServiceWPF.ExportSchetFactureFile
 
         }
 
-        private void ItogReestrXls(string path, string smoName, DateTime currentDate, CancellationToken token)
+        private void ItogReestrXls(string path, string smoData, DateTime currentDate, CancellationToken token)
         {
+            var smoCode = smoData.Split('-')[0];
+            var smoName = smoData.Split('-')[1];
+
             var dtReestr = new DataTable();
             using (var conn = new OracleConnection(AppConfig.Property.ConnectionString))
             {
-                using (var oda = new OracleDataAdapter($"select * from v_lpu_sum_2021 t where smo = {smoName}", conn))
+                using (var oda = new OracleDataAdapter($"select * from v_lpu_sum_2021 t where smo = {smoCode}", conn))
                 {
                     dispatcher.Invoke(() => { Progress1.Text = "Запрос данных v_lpu_sum_2021"; });
                     oda.Fill(dtReestr);
@@ -202,12 +209,78 @@ namespace ClientServiceWPF.ExportSchetFactureFile
                 try
                 {
                     var moCode = row["code_mo"];
-                    var fileName = $@"{path}\{smoName} Итоговый реестр {moCode} за {currentDate.ToString("MMMM yyyy")}.xlsx";
+                    var fileName = $@"{path}\{smoCode} Итоговый реестр {moCode} за {currentDate.ToString("MMMM yyyy")}.xlsx";
 
+                    moCode = "750001";
+                    var dtMo = new DataTable();
+                    using (var conn = new OracleConnection(AppConfig.Property.ConnectionString))
+                    {
+                        using (var oda = new OracleDataAdapter($"select * from nsi.f003 t where mcod = '{moCode}'", conn))
+                        {
+                            dispatcher.Invoke(() => { Progress1.Text = "Запрос данных nsi.f003"; });
+                            oda.Fill(dtMo);
+                        }
+                    }
+                    
+                    int rIndexDtMo = dtMo.Rows.IndexOf(dtMo.Select($" mcod = {moCode}")[0]);
+                    string moName = Convert.ToString(dtMo.Rows[rIndexDtMo]["nam_mok"]);
+                    
                     File.Copy(ITOG_REESTR_TEMPLATE, fileName, true);
                     using (var efm = new ExcelOpenXML())
                     {
                         efm.OpenFile(fileName, 0);
+                        
+                        uint rowIndex = 3;
+                        efm.PrintCell(rowIndex, 1, $"медицинских услуг, оказанных {moName}", null);
+                        rowIndex += 2;
+                        efm.PrintCell(rowIndex, 1, $"гражданам, застрахованным в  {smoName}", null);
+                        rowIndex += 2; 
+                        efm.PrintCell(rowIndex, 1, $"Согласно прилагаемым реестрам, за {currentDate.ToString("MMMM")} оказаны медицинские услуги на сумму:", null);
+                        rowIndex += 2;
+
+                        efm.PrintCell(rowIndex, 2, row["amb_sum"].ToString(), null);
+                        rowIndex++;
+                        efm.PrintCell(rowIndex, 2, row["amb_mbt_sum"].ToString(), null);
+                        rowIndex++;
+                        efm.PrintCell(rowIndex, 2, row["fap_sum"].ToString(), null);
+                        rowIndex++;
+                        efm.PrintCell(rowIndex, 2, row["smp_sum"].ToString(), null);
+                        rowIndex++; 
+                        efm.PrintCell(rowIndex, 2, row["tromb"].ToString(), null);
+                        rowIndex += 2;
+
+                        efm.PrintCell(rowIndex, 2, row["pol"].ToString(), null);
+                        rowIndex++;
+                        efm.PrintCell(rowIndex, 2, row["dializ_a"].ToString(), null);
+                        rowIndex++;
+                        efm.PrintCell(rowIndex, 2, row["rnk"].ToString(), null);
+                        rowIndex++;
+                        efm.PrintCell(rowIndex, 2, row["dli"].ToString(), null);
+                        rowIndex++;
+                        efm.PrintCell(rowIndex, 2, row["neot"].ToString(), null);
+                        rowIndex += 2;
+                        
+                        efm.PrintCell(rowIndex, 2, row["stac"].ToString(), null);
+                        rowIndex++;
+                        efm.PrintCell(rowIndex, 2, row["dializ_c"].ToString(), null);
+                        rowIndex += 2;
+                        
+                        efm.PrintCell(rowIndex, 2, row["ds"].ToString(), null);
+                        rowIndex++;
+                        efm.PrintCell(rowIndex, 2, row["eko"].ToString(), null);
+                        rowIndex += 2;  
+                        
+                        efm.PrintCell(rowIndex, 2, row["hmp"].ToString(), null);
+                        rowIndex += 2;
+
+                        efm.PrintCell(rowIndex, 2, row["disp"].ToString(), null);
+                        rowIndex++; 
+                        efm.PrintCell(rowIndex, 2, row["ud"].ToString(), null);
+                        rowIndex++;
+                        efm.PrintCell(rowIndex, 2, row["other"].ToString(), null);
+                        rowIndex += 2;
+
+                        efm.PrintCell(rowIndex, 2, row["s_all"].ToString(), null);
 
                         efm.MarkAsFinal(true);
                         efm.Save();
